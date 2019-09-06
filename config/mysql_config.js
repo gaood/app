06070200e9635d14f08aa7f8569e98949069fb4a
gaood <gaood123@163.com>
2019-09-06 21:12:25 +0800

    v1.0

--- config/mysql_config.js
+++ config/mysql_config.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 const MYSQL_CONFIG = {
     user: 'root',
-    password: '',
-    database: 'myapp',
-    host: 'localhost',
-    port: '3306'
+    password: 'gaodi0226',
+    database: 'stock_helper',
+    host: 'sh-cdb-ehruedd5.sql.tencentcdb.com',
+    port: '61670'
 }
 const SECRET = 'gaood-123'
 module.exports = {
