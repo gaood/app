c044157501fafcece13a2b894d72c24262cb21e1
gaood <gaood123@163.com>
2019-12-19 01:10:54 +0800

    上传

--- config/mysql_config.js
+++ config/mysql_config.js
@@ -2,14 +2,18 @@ const mysql = require('mysql')
 
 const MYSQL_CONFIG = {
     user: 'root',
-    //password: 'gaodi0226',
-    password: '',
-    //database: 'stock_helper',
-    database: 'myapp',
-    //host: 'sh-cdb-ehruedd5.sql.tencentcdb.com',
-    host: 'localhost',
-    port:'3306'
-    //port: '61670'
+    password: 'gaodi0226',
+    //password: '',
+    database: 'stock_helper',
+    //database: 'myapp',
+    host: 'sh-cdb-ehruedd5.sql.tencentcdb.com',
+    //host: 'localhost',
+    //port:'3306',
+    port: '61670',
+    connectionLimit : 1000,
+    connectTimeout  : 60 * 60 * 1000,
+    acquireTimeout  : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
 }
 const SECRET = 'gaood-1123'
 
