e07caf977934b5ad3650ab929979751f41291846
gaood <gaood123@163.com>
2019-12-20 18:22:00 +0800

    test

--- controllers/home.js
+++ controllers/home.js
@@ -18,35 +18,11 @@ class HomeCtl {
         ctx.body = { url:  `http://123.206.230.76/uploads/${basename}` };
     }
     async test(ctx){
-        multer({
-            dest:'upload'   //upload文件如果不存在则会自己创建一个。
-        }).single('file'),(req,res,next)=>{
-            if (req.file.length==0) {
-                res.render("error",{
-                    message:"上传文件不能为空"
-                })
-                return;
-            } else {
-
-                const file = req.file;
-                const fileInfo = {};
-                console.log(file);
-                //这里修改文件名字，比较随意
-                fs.readFileSync('./upload/'+file.filename,'./upload/'+file.originalname)
-                
-                fileInfo.mimetype = file.mimetype;
-                fileInfo.originalname = file.originalname;
-                fileInfo.size = file.size;
-                fileInfo.path = file.path;
-
-                // 设置响应类型及编码
-                res.set({
-                    'content-type': 'application/json; charset=utf-8'
-                });
-
-                res.end("上传成功！");
-            }
-        }
+        mconsole.log("########----test----############")
+        await query(QUERY_TABLE(user_articles)).then((res)=>{
+          console.log("查询")
+          console.log(res)
+        })
     
     }
 
